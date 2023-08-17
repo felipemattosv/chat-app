742516d87f74d62b29deed2495fdc45bb93544c1
felipemattosv <felipe.vanetti@gmail.com>
2023-08-17 11:25:05 -0300

    add: register authentication

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable react/no-children-prop */
+import { useState } from 'react';
+
 import { ChatIcon, LockIcon, EmailIcon } from '@chakra-ui/icons';
 import {
   Flex,
@@ -9,10 +11,62 @@ import {
   Button,
   Text,
   Link,
+  useToast,
 } from '@chakra-ui/react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+import { auth } from '../services/firebase-config';
 
 const Register: NextPage = () => {
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+
+  const router = useRouter();
+
+  const toast = useToast();
+
+  async function RegisterDefault() {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      router.push('./chat');
+
+      toast({
+        title: `Welcome ${user.user?.email}`,
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      });
+    } catch (error) {
+      let errorMessage = 'Error';
+
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+      if (emailRegex.test(registerEmail) === false) {
+        errorMessage = 'Invalid email';
+      } else if (registerPassword.length < 6) {
+        errorMessage = 'Password not accepted';
+      } else {
+        errorMessage = 'Email already in use';
+      }
+
+      toast({
+        title: errorMessage,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+
+      setRegisterEmail('');
+      setRegisterPassword('');
+    }
+  }
+
   return (
     <Flex h="100vh" justify="center" align="center" flexDir="column">
       <ChatIcon boxSize={16} color="#8800C7" />
@@ -25,6 +79,8 @@ const Register: NextPage = () => {
           placeholder="Enter your email"
           color="white"
           _hover={{ opacity: '0.5' }}
+          value={registerEmail}
+          onChange={(e) => setRegisterEmail(e.target.value)}
         />
       </InputGroup>
       <InputGroup width="14rem" mt="1rem">
@@ -37,16 +93,22 @@ const Register: NextPage = () => {
           placeholder="Enter your password"
           color="white"
           _hover={{ opacity: '0.5' }}
+          value={registerPassword}
+          onChange={(e) => setRegisterPassword(e.target.value)}
+          focusBorderColor={registerPassword.length < 6 ? 'red.900' : ''}
         />
       </InputGroup>
       <Text w="14rem" color="red.900">
-        Password must be at least 6 characters long
+        {registerPassword.length < 6
+          ? 'Password must be at least 6 characters long'
+          : ''}
       </Text>
       <Button
         mt="2rem"
         variant="outline"
         color="white"
         _hover={{ opacity: '0.5' }}
+        onClick={() => RegisterDefault()}
       >
         Register
       </Button>
