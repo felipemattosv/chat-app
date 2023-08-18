0eb1c4541d3d0fc8f3a2ca6361b4225f9b9b5080
felipemattosv <felipe.vanetti@gmail.com>
2023-08-18 20:00:56 -0300

    add: ChatHeader functionality

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable react/function-component-definition */
+import { useState } from 'react';
+
 import { Flex } from '@chakra-ui/react';
+import { onAuthStateChanged } from 'firebase/auth';
 import { NextPage } from 'next';
 
 import { ChatHeader } from '../components/ChatHeader';
+import { auth } from '../services/firebase-config';
 
 const Chat: NextPage = () => {
+  const [userEmail, setUserEmail] = useState('');
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUserEmail(currentUser?.email ?? '');
+  });
   return (
     <Flex justify="center" align="center" h="100vh" w="100vw" flexDir="column">
-      <ChatHeader />
+      <ChatHeader userEmail={userEmail} />
     </Flex>
   );
 };
